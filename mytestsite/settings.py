4477d479ae64e967f22acbc534a1e4072289a1da
PPIICCAA <abdulhaidrees@gmail.com>
2022-05-10 11:37:24 +0500

    heroku

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+#DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
